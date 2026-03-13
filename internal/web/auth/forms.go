e6f0ae98a19d220d38162ed092d55485df4ea7d1
Louis Hamelers <louis@hamelers.nl>
2026-03-13 17:20:17 +0100

    shuffle some things

--- internal/web/auth/forms.go
+++ internal/web/auth/forms.go
@@ -2,6 +2,17 @@ package auth
 
 import "github.com/pocketbase/pocketbase/core"
 
+type RegisterFormValue struct {
+	Email          string
+	Password       string
+	PasswordRepeat string
+}
+
+type LoginFormValue struct {
+	Email    string
+	Password string
+}
+
 func GetLoginFormValue(e *core.RequestEvent) LoginFormValue {
 	return LoginFormValue{
 		Email:    e.Request.FormValue("email"),
